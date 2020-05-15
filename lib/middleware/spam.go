aa5434e4bfef2c505b59eaedf7dd067cfc964c41
bigo <bigodines@gmail.com>
2020-05-14 18:50:33 -0700

    email regexp

--- lib/middleware/spam.go
+++ lib/middleware/spam.go
@@ -3,7 +3,12 @@ package middleware
 import (
 	"fmt"
 	"github.com/gempir/go-twitch-irc/v2"
+	"regexP"
 )
+
+// email validation regexp by W3C
+// https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemail
+var SpamEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func Spam() func(twitch.Message) error {
 	return func(m twitch.Message) error {
