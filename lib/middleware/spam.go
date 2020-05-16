877954170af745319ade03e3ee07c5952b294beb
bigo <bigodines@gmail.com>
2020-05-15 19:56:08 -0700

    coming together. slowly

--- lib/middleware/spam.go
+++ lib/middleware/spam.go
@@ -3,15 +3,23 @@ package middleware
 import (
 	"fmt"
 	"github.com/gempir/go-twitch-irc/v2"
-	"regexP"
+	"regexp"
 )
 
+type (
+	Cmd struct{}
+)
+
+// implement the Command interface
+func (c *Cmd) Execute() {
+}
+
 // email validation regexp by W3C
-// https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemail
+// https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemailconst
 var SpamEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-func Spam() func(twitch.Message) error {
-	return func(m twitch.Message) error {
+func Spam() func(twitch.Message, chan Cmd) error {
+	return func(m twitch.Message, ch chan Cmd) error {
 		fmt.Printf("%d", m.GetType())
 		return nil
 	}
--- lib/middleware/spam_test.go
+++ lib/middleware/spam_test.go
@@ -1,10 +1,17 @@
 package middleware
 
 import (
+	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestUse(t *testing.T) {
+	msg := twitch.PrivateMessage{}
+	cmd := make(chan Cmd, 1)
+	middleware := Spam()
+	err := middleware(&msg, cmd)
+	assert.Nil(t, err)
+
 	assert.True(t, true)
 }
