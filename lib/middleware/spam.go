0c9d338701f9c42bbafa1910fe9259d63109f6b9
bigo <bigodines@gmail.com>
2020-05-20 22:08:29 -0700

    cosmetics

--- lib/middleware/spam.go
+++ lib/middleware/spam.go
@@ -17,7 +17,7 @@ func (c *Cmd) Execute() error {
 }
 
 // email validation regexp by W3C
-// https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemailconst
+// https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemail
 var SpamEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func Spam() func(twitch.Message, chan lib.Command) error {
